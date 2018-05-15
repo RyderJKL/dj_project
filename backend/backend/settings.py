256a9c2f00bf438d414c38d796a67b06a80d11da
chenrong <chenrong@lingxi360.com>
2018-05-15 23:25:49 +0800

    feat: add dj_project models

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import sys
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 sys.path.insert(0, BASE_DIR)
 # sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
